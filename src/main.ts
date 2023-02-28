f224216655733aa872c643d7643be4f668e48c4e
til2to <padwusup@gmail.com>
2023-02-28 17:09:40 +0000

    Add files

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { ApolloServer } from 'apollo-server';
+
+import typeDefs from './schema';
+import resolvers from './resolvers';
+
+const server = new ApolloServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers
+});
+
+server.listen({ port: process.env.PORT || 3000 }).then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+});
